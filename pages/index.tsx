d5130634e696434c5c57dfc7b4d1cca5579e0035
Sergio Cano <canosergio90@gmail.com>
2023-01-22 22:01:32 +0100

    av
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,51 +4,16 @@ import { Layout } from '../components/ui'
 import { useFlashcardSet } from '../hooks'
 import { IFlashCard } from '../interfaces'
 
-const flashcards: IFlashCard[] = [
-  {
-    id: 1,
-    front: 'Ich möchte ein Bier',
-    back: 'I want a beer',
-    tags: ['social'],
-  },
-  { id: 2, front: 'Pass du auf', back: 'take care', tags: ['social'] },
-  {
-    id: 3,
-    front: 'Ich möchte ein Bier',
-    back: 'I want a beer',
-    tags: ['social'],
-  },
-  { id: 4, front: 'Pass du auf', back: 'take care', tags: ['social'] },
-  {
-    id: 5,
-    front: 'Ich möchte ein Bier',
-    back: 'I want a beer',
-    tags: ['social'],
-  },
-  { id: 6, front: 'Pass du auf', back: 'take care', tags: ['social'] },
-  {
-    id: 7,
-    front: 'Ich möchte ein Bier',
-    back: 'I want a beer',
-    tags: ['social'],
-  },
-  { id: 8, front: 'Pass du auf', back: 'take care', tags: ['social'] },
-  {
-    id: 9,
-    front: 'Ich möchte ein Bier',
-    back: 'I want a beer',
-    tags: ['social'],
-  },
-  { id: 10, front: 'Pass du auf', back: 'take care', tags: ['social'] },
-]
-
 const HomePage: NextPage = () => {
   const { flashcardSet, isLoading } = useFlashcardSet()
 
+  const { flashcards } = flashcardSet
+
   return (
-    <Layout title={'m-flashcards'}>
-      <FlashcardList flashcards={flashcards} />
-    </Layout>
+    { flashcards && (<Layout title={'m-flashcards'}>
+    <FlashcardList flashcards={flashcards} />
+  </Layout>)}
+    
   )
 }
 
