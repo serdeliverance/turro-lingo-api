71a1fb35fdd2d9a03d9127c62763125920c2e536
Sergio Cano <canosergio90@gmail.com>
2023-03-05 16:15:10 +0100

    adding comments
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- scripts/migrate_txt_to_json.py
+++ scripts/migrate_txt_to_json.py
@@ -8,7 +8,8 @@ So, we can see that example section is optional (that why it is on [] ).
 
 Also, lines that starts with # are ignored. That is to allow you to make comments in the file.
 """
-def migrate_txt_to_json(source_path, deck_name, target_directory):
-    # TODO if destiny path is not specified, it targets to current directory
+def migrate_txt_to_json(source_filename, deck_name, target_directory):
+    # TODO by default, it looks for source_filename in public/data folder
+    # TODO target_directory is optional, if not specified it targets to current directory
     # TODO add explanation of different parameters in the python doc comment and in readme
     pass
