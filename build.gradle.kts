17f9aaf5452751ed3a98aa5683eaca75fbe8fdc9
Sergio Cano <canosergio90@gmail.com>
2023-03-05 13:20:21 +0100

    removing kotlin (#22)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,8 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
+    id("java")
     id("org.springframework.boot") version "3.0.2"
     id("io.spring.dependency-management") version "1.1.0"
     id("com.diffplug.spotless") version "6.16.0"
-    kotlin("jvm") version "1.7.22"
-    kotlin("plugin.spring") version "1.7.22"
 }
 
 group = "io.github.sd3v"
@@ -21,31 +18,23 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
-    }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-configure<com.diffplug.gradle.spotless.SpotlessExtension> {
-    kotlin {
-        ktlint()
-    }
-    kotlinGradle {
-        target("*.gradle.kts")
-        ktlint()
+spotless {
+    java {
+        target(fileTree(rootDir) {
+            include("src/**/*.java")
+            exclude("src/jooq")
+        })
+        removeUnusedImports()
+        googleJavaFormat("1.15.0")
     }
 }
--- src/main/kotlin/io/github/sd3v/mflashcardsbe/api/dto/MenuItemDto.kt
+++
@@ -1,3 +0,0 @@
-package io.github.sd3v.mflashcardsbe.api.dto
-
-data class MenuItemDto(val id: String, val name: String)
