589c65a450dd199ba933b7b5157ee1dcad8be6bb
Sergio Cano <canosergio90@gmail.com>
2023-03-05 13:37:04 +0100

    fix cors
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/main/java/io/github/sd3v/mflashcardsbe/config/AppConfig.java
+++ src/main/java/io/github/sd3v/mflashcardsbe/config/AppConfig.java
@@ -4,6 +4,8 @@ import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -11,5 +13,15 @@ public class AppConfig {
   @Bean
   public MongoClient mongoClient() {
     return MongoClients.create();
+  }
+
+  @Bean
+  public WebFluxConfigurer webFluxConfigurer() {
+    return new WebFluxConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:7700");
+      }
+    };
   }
 }
