7daf673e1770cf433d38d60f3959c22e0415b554
Sergio Cano <canosergio90@gmail.com>
2023-03-07 21:40:40 +0100

    migration script
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- scripts/migrateTxtToJson.sh
+++ scripts/migrateTxtToJson.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-python3 migrate_txt_to_json.py
+# IMPORTANT: run this script from project ROOT folder
+
+# TODO add param validation / check amount of params
+SOURCE_DIR=$PWD/ui/public/data/$1
+TARGET_DIR=$PWD/ui/public/data
+
+python3 scripts/migrate_txt_to_json.py $SOURCE_DIR $TARGET_DIR $2
--- scripts/migrate_txt_to_json.py
+++ scripts/migrate_txt_to_json.py
@@ -1,10 +1,11 @@
 import os.path
 import logging
+import json
+import sys
 
 EN_GB = "en_GB"
 DE_DE = "de_DE"
 
-PUBLIC_DATA_FOLDER = "ui/public/data"
 """Migrates txt vocabulary files to json
 
 Migrates txt vocabulary field to json, so It is easier to insert into db using mongo. It parses lines that have the following format:
@@ -15,31 +16,26 @@ So, we can see that example section is optional (that why it is on [] ).
 
 Also, lines that starts with # are ignored. That is to allow you to make comments in the file.
 """
-def migrate_txt_to_json(filename, deck_name, target_directory = PUBLIC_DATA_FOLDER):
+def migrate_txt_to_json(file_path, target_directory, deck_name, deck_type = "phrases"):
     # TODO by default, it looks for source_filename in public/data folder
     # TODO target_directory is optional, if not specified it targets to current directory
     # TODO add explanation of different parameters in the python doc comment and in readme
-
-    if not filename:
+    # TODO validate file_path is .txt
+    if not file_path:
         # TODO improve this log message
-        logger.info("please provide filename")
+        logger.info("please provide file path")
         return
-
-    # this first implementation looks source_filename at public/data and throws result to it
-    look_up_folder = PUBLIC_DATA_FOLDER
-
-    file_path = os.join(PUBLIC_DATA_FOLDER, filename)
 
     if not file_path:
         logger.info("file not found")
         return
 
-    language = get_language(source_filename)
+    language = get_language(file_path)
 
     flashcard_list = []
 
-    with open(source_filename, 'r') as reader:
-        for line in reader.readLines():
+    with open(file_path, 'r') as file:
+        for line in file:
             if not empty_or_comment(line):
                 flashcard = parse_flashcard_line(line)
                 if flashcard is not None:
@@ -48,7 +44,32 @@ def migrate_txt_to_json(filename, deck_name, target_directory = PUBLIC_DATA_FOLD
     if len(flashcard_list) == 0:
         return
 
-    # TODO write file
+    deck = create_deck(deck_name, deck_type, language, flashcard_list)
+
+    json_obj = json.dumps(deck, indent=2)
+    destiny_filename = get_destiny_filename(file_path)
+    destiny_path = os.path.join(target_directory, destiny_filename)
+
+    with open(destiny_path, "w") as output_file:
+        output_file.write(json_obj)
+
+def get_destiny_filename(file_path):
+    return f'{extract_file_name_with_extension(file_path)}.json'
+
+def extract_file_name_with_extension(file_path):
+    filename = file_path.split(os.sep)[-1]
+    idx_dot = filename.rfind(".")
+    return filename[:idx_dot]
+
+
+def create_deck(name, deck_type, language, flashcard_list):
+    deck = {}
+    deck["name"] = name
+    deck["type"] = deck_type
+    deck["language"] = language
+    deck["tags"] = []
+    deck["flashcards"] = flashcard_list
+    return deck
 
 def parse_flashcard_line(line):
     chunks = line.split("|")
@@ -59,16 +80,21 @@ def parse_flashcard_line(line):
     back = chunks[1].strip()
 
     flashcard = {}
-    flashcard.front = front
-    flashcard.back = back
+    flashcard["front"] = front
+    flashcard["back"] = back
 
     if len(chunks) > 2:
-        flashcard.example = chunks[2].strip()
+        flashcard["example"] = chunks[2].strip()
 
     return flashcard
 
 def get_language(filename):
-    EN_GB if ".en" in filename else DE_DE
+    return EN_GB if ".en" in filename else DE_DE
 
 def empty_or_comment(line):
     return not line or line[0] == '#'
+
+if __name__ == "__main__":
+    args = sys.argv
+    # TODO add command check / arguments number validation
+    migrate_txt_to_json(args[1], args[2], args[3])
