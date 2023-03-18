5179ab06afe083dfec264326a5fd5406c0eee260
Sergio Cano <canosergio90@gmail.com>
2023-03-18 12:30:02 +0100

    av
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/main/java/io/github/sd3v/mflashcardsbe/service/MaintenanceService.java
+++ src/main/java/io/github/sd3v/mflashcardsbe/service/MaintenanceService.java
@@ -1,10 +1,16 @@
 package io.github.sd3v.mflashcardsbe.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class MaintenanceService {
+
+    private final DeckService deckService;
+
     public void addIdToFlashcards() {
-        // TODO implement
+        deckService.getAll()
+                .filter(deck -> deck.name().equals("example")) // just for development, delete later
     }
 }
