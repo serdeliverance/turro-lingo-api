785f80570c74ea224d3fdb42a5d1da186c3b1169
Sergio Cano <canosergio90@gmail.com>
2023-05-12 22:39:03 +0200

    adding kotlin (#34)
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id("java")
     id("org.springframework.boot") version "3.0.2"
     id("io.spring.dependency-management") version "1.1.0"
     id("com.diffplug.spotless") version "6.16.0"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
 }
 
 group = "io.github.sd3v"
@@ -17,9 +20,20 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
@@ -27,11 +41,20 @@ tasks.withType<Test> {
 }
 
 spotless {
+    kotlin {
+        ktlint()
+    }
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktlint()
+    }
     java {
-        target(fileTree(rootDir) {
-            include("src/**/*.java")
-            exclude("src/jooq")
-        })
+        target(
+            fileTree(rootDir) {
+                include("src/**/*.java")
+                exclude("src/jooq")
+            },
+        )
         removeUnusedImports()
         googleJavaFormat("1.15.0")
     }
