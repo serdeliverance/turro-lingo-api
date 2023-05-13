f8bcbd32dac3609a7a518684232d6785cd98b5d8
Sergio Cano <canosergio90@gmail.com>
2023-05-13 20:13:16 +0200

    adding create deck script
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

---
+++ scripts/createDeck.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+SCRIPTS_DIR="$(dirname "$0")"
+
+curl -i -X POST \
+          -H 'Content-Type: application/json' \
+          -d @"$SCRIPTS_DIR/deck.json" \
+          http://localhost:8080/api/decks/
+# TODO add error handling if curl command fails
+echo "Deck created"
