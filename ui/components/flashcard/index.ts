678926700b612ebc5f72b9ce5d59877957368437
Sergio Cano <canosergio90@gmail.com>
2023-08-26 17:33:52 +0200

    Basic nouns page  (#46)
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- ui/components/flashcard/index.ts
+++ ui/components/flashcard/index.ts
@@ -1,1 +1,2 @@
 export * from './Deck'
+export * from './Flashcard'
--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -3,8 +3,8 @@ import type { NextPage } from 'next'
 import { Layout } from '../components/ui'
 import { Menu } from '../components/menu'
 import { useMenuItems } from '../hooks'
-import {GetServerSideProps} from "next";
-import MenuService from "../services/menu.service";
+import { GetServerSideProps } from 'next'
+import MenuService from '../services/menu.service'
 
 const HomePage: NextPage = () => {
   const { items, loading } = useMenuItems()
@@ -22,25 +22,25 @@ const HomePage: NextPage = () => {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    try {
-        const items = await MenuService.getMenuItems()
+  try {
+    const items = await MenuService.getMenuItems()
 
-        if (!items) {
-            throw new Error('error retrieving menu items')
-        }
-
-        return {
-            props: { items },
-        }
-    } catch (error) {
-        console.log(error)
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            },
-        }
+    if (!items) {
+      throw new Error('error retrieving menu items')
     }
+
+    return {
+      props: { items },
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
 }
 
 export default HomePage
