e3f809df1ecb9e0369e527dbf8dbbe618c62e642
Sergio Cano <canosergio90@gmail.com>
2025-04-20 13:50:17 +0200

    initial data - wip
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

---
+++ scripts/db/initial_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO deck (id, name, slug, type, description, language, tags) VALUES (1, 'English', 'english-test', 'vocabulary', 'Learning English vocabulary', 'eng', 'vocabulary');
+
+INSERT INTO flashcard (id, front, back, example, deck_id) VALUES (1, 'to dwell', 'pensar mucho sobre algo, generalmente de forma negativa', 'Don’t dwell on your mistakes, just learn from them.', 1);
+-- add more four flashcards more
